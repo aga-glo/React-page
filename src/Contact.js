14e289684b4933508bfee9a7185abeee26b8b06e
Agnieszka <56108700+aga-glo@users.noreply.github.com>
2021-11-10 17:11:48 +0100

    update new look

--- src/Contact.js
+++ src/Contact.js
@@ -7,6 +7,8 @@ class Contact extends Component {
             <h2>Let's catch up!</h2>
             <p>It's nice that you got here. I am the author of this website :) sooo I am looking for a job as a junior frontend developer</p>
             <p>I am open to work  - please contact with me via email: agnieszka.ewa.glowacka@gmail.com or <a href="https://pl.linkedin.com/in/agnieszka-g%C5%82owacka-938b93177">LinkedIn</a>.</p>
+            
+            <img src="https://user-images.githubusercontent.com/56108700/141132453-045018b0-7d37-437a-a690-c4d53579d52a.jpg" alt="pic"></img>
             </div>
         );
     }
