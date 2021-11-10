60ef4a966e073b6d84a74ee70e4d62897f984fe7
Agnieszka <56108700+aga-glo@users.noreply.github.com>
2021-11-10 13:17:58 +0100

    upload files

---
+++ Home.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+
+class Home extends Component {
+    render() {
+        return (
+           
+            <div>
+                <h2>HI! WELCOME!</h2>
+                <p>My name is Agnieszka Głowacka and I am a student of Computer Science at the Cracow University of Technology - Politechnika Krakowska. I already have some commercial IT experience but I plan my career path towards front-end programming.</p>
+                    <p> I would like to invite you to visit my profile on  <a href="https://pl.linkedin.com/in/agnieszka-g%C5%82owacka-938b93177">LinkedIn</a> and <a href="https://github.com/aga-glo">Github</a>.</p>
+
+                    <img src="https://user-images.githubusercontent.com/56108700/141099273-fb937666-4697-4b62-a103-2805426289da.jpg" alt="pic"></img>
+                    <p>This page is created in React, JS, some CSS and HTML as a project ( I mostly focus here on JS so styles are quite poor).</p>
+                 <p>   Here you can find something more UX/UI nicer -<a href="https://aga-glo.github.io/"> CLICK ME!</a>
+                </p>
+            </div>
+        );
+    }
+}
+export default Home;
---
+++ Stuff.js
@@ -0,0 +1,30 @@
+import React, {Component} from "react";
+
+class Stuff extends Component {
+    render() {
+        return (
+            <div>
+                <h2>IT EXPERIENCE AND SOME INFORMATION</h2>
+                <p>PROGRAMING SKILLS:</p>
+                    <p>- basic of JavaScript, React.js, HTML, CSS </p>
+                    <p>- GIT </p>
+                    <p>- basic knowlage of databases </p>
+                    <p>- basic of basic of Redux </p>               
+                <p>IT TOOLS:</p>
+                    <p>- JIRA </p>
+                    <p> - YOUTRACK </p>
+                    <p> - PHOTOSHOP </p> 
+                    <p> - ADOBE XD </p>
+                    <p> - MS O365 </p> 
+                <p>LANGUAGE:</p>
+                    <p>- Polish - native</p>
+                    <p> - English - B2/C1 </p>
+                    
+
+                <p>I started my adventure with IT in 2018, when I get job as an intern. After a few months, I got a job as a Junior Project Manager in a software house in Krakow, where, due to the small size of the company, I had the opportunity to take a closer look at the work of testers and programmers on the frontend and backend. As part of my duties, I had the opportunity to do some simple things using HTML, CSS or JavaScript - which made me interested in this IT department - frontend coding.</p>
+            </div>
+        );
+    }
+}
+
+export default Stuff;
